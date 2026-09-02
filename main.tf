cc6f228fdfa7a0a33e9dd6a803afd2a82a2bea8a
Naveen3990 <sharmivarun3990@gmail.com>
2026-09-02 22:08:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
 
+terraform {
+  backend "s3" {
+    bucket = "varunsujith9966"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-0b6d9d3d33ba97d99"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Terraform-EC2-AutoDeploy"   # changed tag value
+  }
+}
